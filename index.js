0b293a804318148b508550476bc977dea47bc12b
purplcult14 <emmabyblow@gmail.com>
2024-11-07 17:50:57 -0700

    e
    e

---
+++ index.js
@@ -0,0 +1,26 @@
+sheep.addEventListener('click', function(){
+    console.log('meow')
+    document.getElementById('animal').src =
+		'sheep.webp';
+})
+
+bear.addEventListener('click', function(){
+    console.log('bearrrrr')
+		document.getElementById('animal').src =
+        'bear.webp';
+})
+
+moose.addEventListener('click', function(){
+		document.getElementById('animal').src =
+        'moose.webp';
+})
+
+wolf.addEventListener('click', function(){
+    document.getElementById('animal').src =
+    'wolf.webp';
+})
+
+elk.addEventListener('click', function(){
+    document.getElementById('animal').src =
+    'elk.webp';
+})
